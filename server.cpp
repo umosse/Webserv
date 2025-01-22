c43f148a0b06e0145d34350696b9ec6d27c6c5d6
Ulysse Mosse <umosse@bess-f2r1s10.clusters.42paris.fr>
2025-01-22 14:28:42 +0100

    writes are now sends and the entire response simulation is a lot cleaner and put in a string

--- server.cpp
+++ server.cpp
@@ -6,7 +6,7 @@
 /*   By: umosse <umosse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 13:35:03 by umosse            #+#    #+#             */
-/*   Updated: 2025/01/20 14:38:50 by umosse           ###   ########.fr       */
+/*   Updated: 2025/01/22 14:03:03 by umosse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <sys/epoll.h>
+#include <fstream>
 
 #define MAX_EVENTS 42
 
@@ -28,11 +29,10 @@ int	main()
 	int	client_fd;
 	int	epoll_fd;
 	//fill structure for address to bind
-	struct sockaddr_in addr; {};
+	sockaddr_in addr; {};
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(9999);
 	addr.sin_addr.s_addr = inet_addr("0.0.0.0");
-	sockaddr_in client_addr; {};
 	// fill struct for epoll
 	struct epoll_event ev;
 	struct epoll_event events[MAX_EVENTS];
@@ -89,6 +89,7 @@ int	main()
 		{
 			if (events[n].data.fd == socket_fd) //if (new connection)
 			{
+				sockaddr_in client_addr; {};
 				std::cout << "waiting for epoll\n";
 				socklen_t client_addr_len = sizeof(client_addr);
 				//accept a connection (stock the socket into an fd)
@@ -118,6 +119,7 @@ int	main()
 						perror("Error deleting the current connection");
 						continue ;
 					}
+					std::cout << "fd " << events[n].data.fd << " has been closed\n";
 					close(events[n].data.fd);
 					continue ;
 				}
@@ -144,15 +146,29 @@ int	main()
 				if (events[n].events & EPOLLOUT)
 				{
 					//simulate response from server
-					write(events[n].data.fd, "HTTP/1.1 200 OK\r\n", 17);
-					// Content-Type: text/html
-					write(events[n].data.fd, "Content-Type: text/html\r\n", 25);
-					write(events[n].data.fd, "\r\n", 2);
-					write(events[n].data.fd, "<head>\n\t<title>Hello world</title>\n</head>", 43);
-					write(events[n].data.fd, "<h1>Hello world</h1>\n", 22);
-					write(events[n].data.fd, "<p style='color: red;'>This is a paragraph</p>\n", 48);
-					write(events[n].data.fd, "<a href=\"https://www.youtube.com/watch?v=MtN1YnoL46Q&pp=ygUNdGhlIGR1Y2sgc29uZw%3D%3D\">DUCK</a>\n", 96);
-					//close (events[n].data.fd);
+					std::string header;
+					header = "HTTP/1.1 200 OK\r\n"
+					"Content-Type: text/html\r\n"
+					"Content-Length: 604\r\n"
+					"\r\n";
+					send(events[n].data.fd, header.c_str(), header.length(), 0);
+					std::string page;
+					page = "<!DOCTYPE html>"
+					"<html lang=\"en\">"
+					"<head>"
+					"	<meta charset=\"UTF-8\">"
+					"	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+					"	<title>Document</title>"
+					"</head>"
+					"<body>"
+					"	<h1>Hello world</h1>"
+					"	<p style='color: red;'>This is a paragraph</p>"
+					"	<a href=\"https://www.youtube.com/watch?v=MtN1YnoL46Q&pp=ygUNdGhlIGR1Y2sgc29uZw%3D%3D\" target=\"_blank\">DUCK</a>"
+					"	<p></p>"
+					"	<a href=\"https://www.youtube.com/watch?v=zg00AYUEU9s\" target=\"_blank\"><img src=\"https://yt3.googleusercontent.com/OuwCgGAdNqyeyJ8i3JocIXzjHdSBtZzMpnltZqtQBjRE5Xx7RCXO3XTzRgZ_JmKPFxkakbb6Ng=s900-c-k-c0x00ffffff-no-rj\" alt=\"FlexingPenguin\"</a>"
+					"</body>"
+					"</html>";
+					send(events[n].data.fd, page.c_str(), page.length(), 0);
 					struct epoll_event ev;
 					ev.data.fd = events[n].data.fd;
 					ev.events = EPOLLIN | EPOLLRDHUP;
